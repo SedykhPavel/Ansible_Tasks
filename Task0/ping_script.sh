1852834552a4acba55d14c55d50118866fe85c6a
SedykhPavel <159540710+SedykhPavel@users.noreply.github.com>
2026-02-14 14:12:46 +0500

    jopa

--- Task0/ping_script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-while true; do echo "$(date '+%Y-%m-%d %H:%M:%S') | Code: $(curl -s -o /dev/null -w "%{http_code}" https://s3.mdst.yandex.net) | Time: $(curl -s -o /dev/null -w "%{time_total}" https://s3.mdst.yandex.net)s"; sleep 10; done
---
+++ Task0/script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+while true; do echo "$(date '+%Y-%m-%d %H:%M:%S') | Code: $(curl -s -o /dev/null -w "%{http_code}" https://s3.mdst.yandex.net) | Time: $(curl -s -o /dev/null -w "%{time_total}" https://s3.mdst.yandex.net)s"; sleep 5; done
